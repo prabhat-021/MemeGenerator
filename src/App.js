31031e6f3f33bbad71f1f2bd3e87feacde7b3373
Prabhat Sahrawat <97847933+prabhat-021@users.noreply.github.com>
2022-09-15 02:45:18 +0530

    meme section added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 import Header from "./Header.js";
+import Meme from "./Meme.js";
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   );
 }
---
+++ src/Meme.js
@@ -0,0 +1,7 @@
+export default function Meme() {
+  return (
+    <section className="meme">
+      <h1>meme</h1>
+    </section>
+  );
+}
