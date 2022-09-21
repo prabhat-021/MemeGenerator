2097b11f3864fbb5932dfd05edb134b4eda6f685
Prabhat Sahrawat <97847933+prabhat-021@users.noreply.github.com>
2022-09-21 23:57:09 +0530

    updated

--- src/Meme.js
+++ src/Meme.js
@@ -1,7 +1,11 @@
 export default function Meme() {
   return (
     <section className="meme">
-      <h1>meme</h1>
+      <div className="form">
+        <input type="text" placeholder="Top text" className="form-input" />
+        <input type="text" placeholder="Bottom text" className="form-input" />
+        <button className="form-btn"> Get a new meme image </button>
+      </div>
     </section>
   );
 }
