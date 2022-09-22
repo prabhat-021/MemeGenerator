33a5f08201264fdb9a21e95ad693ee81c057a83e
Prabhat Sahrawat <97847933+prabhat-021@users.noreply.github.com>
2022-09-23 00:34:22 +0530

    Memedata updated

--- src/Meme.js
+++ src/Meme.js
@@ -1,10 +1,33 @@
+import React, { useState } from "react";
+import memesData from "./MemeData.js";
+
 export default function Meme() {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg"
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
+  function getMemeImage() {
+    const memesArray = allMemeImages.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url
+    }));
+  }
   return (
     <section className="meme">
       <div className="form">
         <input type="text" placeholder="Top text" className="form-input" />
         <input type="text" placeholder="Bottom text" className="form-input" />
-        <button className="form-btn"> Get a new meme image </button>
+        <button className="form-btn" onClick={getMemeImage}>
+          {" "}
+          Get a new meme image{" "}
+        </button>
+        <img src={meme.randomImage} alt="" className="meme-image" />
       </div>
     </section>
   );
